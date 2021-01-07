3f8bdadafdfb49d5c782bd16b99907c6c26a5cd8
aruald <s104004@ap.be>
2021-01-07 14:38:51 +0100

    startscene added

---
+++ DogTrainer/Assets/Scripts/StartGameScript.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class StartGameScript : MonoBehaviour
+{
+
+    public string sceneName;
+    public Button loadSceneBtn;
+    // Start is called before the first frame update
+    void Start()
+    {
+        loadSceneBtn.onClick.AddListener(ChangeScene);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    
+     void ChangeScene() 
+    {
+        SceneManager.LoadSceneAsync(sceneName);
+    
+    }
+
+    
+}
