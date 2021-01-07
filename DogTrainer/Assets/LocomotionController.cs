b1e517af2e7bdbd486d7419cef6f12a331aebaea
aruald <s104004@ap.be>
2021-01-07 16:07:48 +0100

    tryout buttonclick

---
+++ DogTrainer/Assets/LocomotionController.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class LocomotionController : MonoBehaviour
+{
+    public XRController leftteleportRay;
+    public XRController rightteleportRay;
+    public InputHelpers.Button teleportActivationButton;
+    public float activationThreshold = 0.1f;
+
+    public XRRayInteractor leftInteractorRay;
+
+    public bool EnableLeftTeleport { get; set; } = true;
+    public bool EnableRightTeleport { get; set; } = true;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 pos = new Vector3();
+        Vector3 norm = new Vector3();
+        int index = 0;
+        bool validtarget = false;
+
+        if (leftteleportRay)
+        {
+            bool isLeftInteractorRayHovering = leftInteractorRay.TryGetHitInfo(ref pos, ref norm, ref index, ref validtarget);
+            leftteleportRay.gameObject.SetActive(EnableLeftTeleport && CheckIfActivated(leftteleportRay) && !isLeftInteractorRayHovering);
+
+        }
+    }
+
+    public bool CheckIfActivated(XRController controller)
+    {
+        InputHelpers.IsPressed(controller.inputDevice, teleportActivationButton, out bool isActivated, activationThreshold);
+        return isActivated;
+    }
+}
